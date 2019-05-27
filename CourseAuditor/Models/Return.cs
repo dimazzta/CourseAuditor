5ca0770a98d31f1b85810cf472b3b4ffde8bb2a8
Ployka <grinenkoivan98@mail.ru>
2019-05-28 01:27:46 +0300

    Return

--- CourseAuditor/Models/Return.cs
+++ CourseAuditor/Models/Return.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,11 +11,59 @@ namespace CourseAuditor.Models
 {
     public class Return
     {
-        public int Id { get; set; }
+        private int id;
+        private int studentId;
+        private DateTime date;
+        private double sum;
+
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
         [Column("Student_ID")]
-        public int StudentId { get; set; }
+        public int StudentId
+        {
+            get { return studentId; }
+            set
+            {
+                studentId = value;
+                OnPropertyChanged("StudentId");
+            }
+        }
         public virtual Student Student { get; set; }
-        public DateTime Date { get; set; }
-        public double Sum { get; set; }
+
+        public DateTime Date
+        {
+            get { return date; }
+            set
+            {
+                date = value;
+                OnPropertyChanged("Date");
+            }
+        }
+        public double Sum
+        {
+            get { return sum; }
+            set
+            {
+                sum = value;
+                OnPropertyChanged("Sum");
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }

1785e9551f8d8a47c5e79eb010bf23edb852e37e
Ployka <grinenkoivan98@mail.ru>
2019-05-28 01:42:30 +0300

    PersonParent

--- CourseAuditor/Models/PersonParent.cs
+++ CourseAuditor/Models/PersonParent.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,12 +12,48 @@ namespace CourseAuditor.Models
     [Table("Person_Parent")]
     public class PersonParent
     {
-        public int Id { get; set; }
+        private int id;
+        private int personId;
+        private int parentId;
+
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
         [Column("Person_ID")]
-        public int PersonId { get; set; }
+        public int PersonId
+        {
+            get { return personId; }
+            set
+            {
+                personId = value;
+                OnPropertyChanged("PersonId");
+            }
+        }
         public virtual Person Person { get; set; }
         [Column("Parent_ID")]
-        public int ParentId { get; set; }
+        public int ParentId
+        {
+            get { return parentId; }
+            set
+            {
+                parentId = value;
+                OnPropertyChanged("ParentId");
+            }
+        }
         public virtual Parent Parent { get; set; }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }

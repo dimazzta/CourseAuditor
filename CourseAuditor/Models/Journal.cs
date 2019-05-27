f8375b01c6ae71e8b959671585afce056d2ef610
Ployka <grinenkoivan98@mail.ru>
2019-05-27 15:10:11 +0300

    Record part of model

---
+++ CourseAuditor/Models/Journal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CourseAuditor.Models
+{
+    public class Journal
+    {
+        public int Id { get; set; }
+
+        public int StudentId { get; set; }
+        public virtual Student Student { get; set; }
+
+        public DateTime Date { get; set; }
+        public string Attendance_assessment { get; set; }
+    }
+}

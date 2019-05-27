aeccef532da18d4440f893d617041814ee2827db
Ployka <grinenkoivan98@mail.ru>
2019-05-28 02:02:23 +0300

    Discount

--- CourseAuditor/Models/Payment.cs
+++ CourseAuditor/Models/Payment.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,12 +11,66 @@ namespace CourseAuditor.Models
 {
     public class Payment
     {
-        public int Id { get; set; }
+        private int id;
+        private int studentId;
+        private double sum;
+        private DateTime date;
+        private double? discount;
+
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
         [Column("Student_ID")]
-        public int StudentId { get; set; }
+        public int StudentId
+        {
+            get { return studentId; }
+            set
+            {
+                studentId = value;
+                OnPropertyChanged("StudentId");
+            }
+        }
         public virtual Student Student { get; set; }
-        public double Sum { get; set; }
-        public DateTime Date { get; set; }
-        public double? Discount { get; set; }
+        public double Sum
+        {
+            get { return sum; }
+            set
+            {
+                sum = value;
+                OnPropertyChanged("Sum");
+            }
+        }
+        public DateTime Date
+        {
+            get { return date; }
+            set
+            {
+                date = value;
+                OnPropertyChanged("Date");
+            }
+        }
+        public double? Discount
+        {
+            get { return discount; }
+            set
+            {
+                discount = value;
+                OnPropertyChanged("Discount");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }

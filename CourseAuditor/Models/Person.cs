edd64aecd296c0803483f3463fee656eef248a28
Ployka <grinenkoivan98@mail.ru>
2019-05-28 01:49:33 +0300

    Person

--- CourseAuditor/Models/Person.cs
+++ CourseAuditor/Models/Person.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,13 +11,58 @@ namespace CourseAuditor.Models
 {
     public class Person
     {
-        public int Id { get; set; }
+        private int id;
+        private string firstName;
+        private string secondName;
+        private string patronymic;
+
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
         [Column("First_Name")]
-        public string FirstName { get; set; }
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                firstName = value;
+                OnPropertyChanged("FirstName");
+            }
+        }
         [Column("Second_Name")]
-        public string SecondName { get; set; }
-        public string Patronymic { get; set; }
+        public string SecondName
+        {
+            get { return secondName; }
+            set
+            {
+                secondName = value;
+                OnPropertyChanged("SecondName");
+            }
+        }
+        public string Patronymic
+        {
+            get { return patronymic; }
+            set
+            {
+                patronymic = value;
+                OnPropertyChanged("Patronymic");
+            }
+        }
 
         public virtual ICollection<PersonParent> Parents { get; set; }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }

257624313a8af174c7cec61b8522f15ce61c143e
ElvgOvadykov <oelvg@mail.ru>
2019-06-02 18:46:31 +0300

    Add SelectedGroup in MainVM

--- CourseAuditor/ViewModels/MainVM.cs
+++ CourseAuditor/ViewModels/MainVM.cs
@@ -15,12 +15,14 @@ namespace CourseAuditor.ViewModels
         public ObservableCollection<Course> Courses { get; set; }
 
         public ObservableCollection<Student> Students { get; set; }
+        public Group SelectedGroup { get; set; }
 
         public MainVM()
         {
             _context = new ApplicationContext();
             Courses = new ObservableCollection<Course>(_context.Courses);
             Students = new ObservableCollection<Student>(_context.Students);
+            SelectedGroup = new Group();
         }
     }
 }
